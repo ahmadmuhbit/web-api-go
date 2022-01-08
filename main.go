12cf652e3c1250e092e6cbb4a0a959469220beb4
ahmadmuhbit <ahmadmuhbit@gmail.com>
2022-01-08 20:23:00 +0700

    Path Variable dan Query String

--- main.go
+++ main.go
@@ -10,19 +10,39 @@ func main() {
 
 	router := gin.Default()
 
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"name": "Ahmad Muhbit",
-			"bio":  "Backend Engineer & Digital Marketing",
-		})
-	})
+	router.GET("/", rootHandler)
+	router.GET("/hello", helloHandler)
+	router.GET("/books/:id/:title", booksHandler)
+	router.GET("/query", queryHandler)
 
-	router.GET("/hello", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"title":    "Hello World",
-			"subtitle": "Belajar Golang bareng Agung Setiawan",
-		})
-	})
-	router.Run(":8888")
+	router.Run()
 
+}
+
+func rootHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"name": "Ahmad Muhbit",
+		"bio":  "Backend Engineer & Digital Marketing",
+	})
+}
+
+func helloHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"title":    "Hello World",
+		"subtitle": "Belajar Golang bareng Agung Setiawan",
+	})
+}
+
+func booksHandler(c *gin.Context) {
+	id := c.Param("id")
+	title := c.Param("title")
+
+	c.JSON(http.StatusOK, gin.H{"id": id, "title": title})
+}
+
+func queryHandler(c *gin.Context) {
+	title := c.Query("title")
+	price := c.Query("price")
+
+	c.JSON(http.StatusOK, gin.H{"title": title, "price": price})
 }
