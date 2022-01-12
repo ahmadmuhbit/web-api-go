0a1218c3ef7ae471aefe69280de4a93a45e974c0
ahmadmuhbit <ahmadmuhbit@gmail.com>
2022-01-12 07:53:46 +0700

    Koneksi Database

--- main.go
+++ main.go
@@ -1,12 +1,25 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"pustaka-api/handler"
 
 	"github.com/gin-gonic/gin"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
+
+	dsn := "root:root@tcp(127.0.0.1:8889)/pustaka-api?charset=utf8mb4&parseTime=True&loc=Local"
+	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Db connection error")
+	}
+
+	fmt.Println("Database connection success")
 
 	router := gin.Default()
 
